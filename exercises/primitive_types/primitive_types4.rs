96ee822aa00b7c8530f62c99e7e71d55d5749158
swordtraveller <39628669+swordtraveller@users.noreply.github.com>
2024-04-09 13:26:04 +0000

    update primitive_types4

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -5,13 +5,12 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // 这里&表示引用，切片只能分配到堆上，因此要通过一个在栈上的引用来访问堆上数据
+    let nice_slice = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
