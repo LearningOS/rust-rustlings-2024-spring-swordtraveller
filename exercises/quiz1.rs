07e2fbd82c22d31bc87de10d64dda01468c23fe8
swordtraveller <39628669+swordtraveller@users.noreply.github.com>
2024-04-09 13:09:09 +0000

    update quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i64) -> i64 {
+    if quantity <= 40 {
+        return quantity * 2;
+    } else {
+        return quantity;
+    }
+}
 
 // Don't modify this function!
 #[test]
