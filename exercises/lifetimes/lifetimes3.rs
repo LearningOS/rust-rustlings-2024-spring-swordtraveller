80c9fd159de0037cd147d0786e9d23ea49d8c501
swordtraveller <39628669+swordtraveller@users.noreply.github.com>
2024-04-25 15:06:15 +0000

    update lifetimes3

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -5,11 +5,9 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'t> {
+    author: &'t str,
+    title: &'t str,
 }
 
 fn main() {
