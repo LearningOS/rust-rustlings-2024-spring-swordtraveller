8035719ec43a1a4d6d31abe9c69b36fee6463777
swordtraveller <39628669+swordtraveller@users.noreply.github.com>
2024-04-27 11:56:30 +0000

    update algorithm7

--- exercises/algorithm/algorithm7.rs
+++ exercises/algorithm/algorithm7.rs
@@ -1,108 +1,133 @@
 /*
-	stack
-	This question requires you to use a stack to achieve a bracket match
+    stack
+    This question requires you to use a stack to achieve a bracket match
 */
 
-// I AM NOT DONE
+use std::collections::HashMap;
+
 #[derive(Debug)]
 struct Stack<T> {
-	size: usize,
-	data: Vec<T>,
+    size: usize,
+    data: Vec<T>,
 }
 impl<T> Stack<T> {
-	fn new() -> Self {
-		Self {
-			size: 0,
-			data: Vec::new(),
-		}
-	}
-	fn is_empty(&self) -> bool {
-		0 == self.size
-	}
-	fn len(&self) -> usize {
-		self.size
-	}
-	fn clear(&mut self) {
-		self.size = 0;
-		self.data.clear();
-	}
-	fn push(&mut self, val: T) {
-		self.data.push(val);
-		self.size += 1;
-	}
-	fn pop(&mut self) -> Option<T> {
-		// TODO
-		None
-	}
-	fn peek(&self) -> Option<&T> {
-		if 0 == self.size {
-			return None;
-		}
-		self.data.get(self.size - 1)
-	}
-	fn peek_mut(&mut self) -> Option<&mut T> {
-		if 0 == self.size {
-			return None;
-		}
-		self.data.get_mut(self.size - 1)
-	}
-	fn into_iter(self) -> IntoIter<T> {
-		IntoIter(self)
-	}
-	fn iter(&self) -> Iter<T> {
-		let mut iterator = Iter { 
-			stack: Vec::new() 
-		};
-		for item in self.data.iter() {
-			iterator.stack.push(item);
-		}
-		iterator
-	}
-	fn iter_mut(&mut self) -> IterMut<T> {
-		let mut iterator = IterMut { 
-			stack: Vec::new() 
-		};
-		for item in self.data.iter_mut() {
-			iterator.stack.push(item);
-		}
-		iterator
-	}
+    fn new() -> Self {
+        Self {
+            size: 0,
+            data: Vec::new(),
+        }
+    }
+    fn is_empty(&self) -> bool {
+        0 == self.size
+    }
+    fn len(&self) -> usize {
+        self.size
+    }
+    fn clear(&mut self) {
+        self.size = 0;
+        self.data.clear();
+    }
+    fn push(&mut self, val: T) {
+        self.data.push(val);
+        self.size += 1;
+    }
+    fn pop(&mut self) -> Option<T> {
+        if self.is_empty() {
+            return None;
+        }
+        self.size = self.size - 1;
+        self.data.pop()
+    }
+    fn peek(&self) -> Option<&T> {
+        if 0 == self.size {
+            return None;
+        }
+        self.data.get(self.size - 1)
+    }
+    fn peek_mut(&mut self) -> Option<&mut T> {
+        if 0 == self.size {
+            return None;
+        }
+        self.data.get_mut(self.size - 1)
+    }
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+    fn iter(&self) -> Iter<T> {
+        let mut iterator = Iter { stack: Vec::new() };
+        for item in self.data.iter() {
+            iterator.stack.push(item);
+        }
+        iterator
+    }
+    fn iter_mut(&mut self) -> IterMut<T> {
+        let mut iterator = IterMut { stack: Vec::new() };
+        for item in self.data.iter_mut() {
+            iterator.stack.push(item);
+        }
+        iterator
+    }
 }
 struct IntoIter<T>(Stack<T>);
 impl<T: Clone> Iterator for IntoIter<T> {
-	type Item = T;
-	fn next(&mut self) -> Option<Self::Item> {
-		if !self.0.is_empty() {
-			self.0.size -= 1;self.0.data.pop()
-		} 
-		else {
-			None
-		}
-	}
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.0.is_empty() {
+            self.0.size = self.0.size - 1;
+            self.0.data.pop()
+        } else {
+            None
+        }
+    }
 }
 struct Iter<'a, T: 'a> {
-	stack: Vec<&'a T>,
+    stack: Vec<&'a T>,
 }
 impl<'a, T> Iterator for Iter<'a, T> {
-	type Item = &'a T;
-	fn next(&mut self) -> Option<Self::Item> {
-		self.stack.pop()
-	}
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
 }
 struct IterMut<'a, T: 'a> {
-	stack: Vec<&'a mut T>,
+    stack: Vec<&'a mut T>,
 }
 impl<'a, T> Iterator for IterMut<'a, T> {
-	type Item = &'a mut T;
-	fn next(&mut self) -> Option<Self::Item> {
-		self.stack.pop()
-	}
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
 }
 
 fn bracket_match(bracket: &str) -> bool
 {
-	//TODO
-	true
+    let map = HashMap::from([(')', '('), (']', '['), ('}', '{')]);
+
+    let mut stack = Stack::new();
+    for char in bracket.chars() {
+        if map.contains_key(&char) {
+            while let optional = stack.pop() {
+                match optional {
+                    None => {
+                        return false;
+                    }
+                    Some(v) => {
+                        if map.values().any(|&val| val == v) {
+                            if v == map[&char] {
+                                break;
+                            } else {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    stack.is_empty()
 }
 
 #[cfg(test)]
