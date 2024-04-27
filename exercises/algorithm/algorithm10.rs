e3cf32b344f9dce4f15b8e5f86d0f3d2f18b795a
swordtraveller <39628669+swordtraveller@users.noreply.github.com>
2024-04-27 12:34:16 +0000

    update algorithm10

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -1,8 +1,7 @@
 /*
-	graph
-	This problem requires you to implement a basic graph functio
+    graph
+    This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -29,7 +28,17 @@ impl Graph for UndirectedGraph {
         &self.adjacency_table
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let mut value = self
+            .adjacency_table
+            .entry(edge.0.to_string())
+            .or_insert(vec![]);
+        value.push((edge.1.into(), edge.2));
+
+        let mut value = self
+            .adjacency_table
+            .entry(edge.1.to_string())
+            .or_insert(vec![]);
+        value.push((edge.0.into(), edge.2));
     }
 }
 pub trait Graph {
@@ -38,7 +47,7 @@ pub trait Graph {
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
         //TODO
-		true
+        true
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
